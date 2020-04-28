892c40ee69d3db18e92712e172138a9260bf49ec
Delia Brown <DELIA.LYNNE@GMAIL.COM>
2020-04-28 19:23:35 -0400

    Initial commit

---
+++ KnotGames.ProgrammingPuzzles/PageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnotGames.ProgrammingPuzzles.Pages
+{
+    public static class PageExtensions
+    {
+        public static string GetPlayerName(this PageModel pageModel) =>
+            pageModel.HttpContext.Session.GetString("Name");
+
+        public static void SetPlayerName(this PageModel pageModel, string name)
+        {
+            pageModel.HttpContext.Session.SetString("Name", name ?? "");
+            pageModel.HttpContext.Session.CommitAsync().Wait();
+        }
+    }
+}
---
+++ KnotGames.ProgrammingPuzzles/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace KnotGames.ProgrammingPuzzles.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            this.SetPlayerName(Name);
+        }
+    }
+}
