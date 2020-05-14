a68bbc2724860f4317f88b95281b279ce2ea08b1
Delia Brown <DELIA.LYNNE@GMAIL.COM>
2020-05-14 12:50:52 -0400

    I finished story 1

--- KnotGames.ProgrammingPuzzles/PageExtensions.cs
+++ KnotGames.ProgrammingPuzzles/PageExtensions.cs
@@ -10,11 +10,20 @@ namespace KnotGames.ProgrammingPuzzles.Pages
     public static class PageExtensions
     {
         public static string GetPlayerName(this PageModel pageModel) =>
-            pageModel.HttpContext.Session.GetString("Name");
+            pageModel.HttpContext.Session.GetString("Name") ?? "No Name";
 
         public static void SetPlayerName(this PageModel pageModel, string name)
         {
             pageModel.HttpContext.Session.SetString("Name", name ?? "");
+            pageModel.HttpContext.Session.CommitAsync().Wait();
+        }
+
+        public static bool GetUnicornMode(this PageModel pageModel) =>
+           pageModel.HttpContext.Session.GetString("Unicorn") == "Yes";
+
+        public static void SetUnicornMode(this PageModel pageModel, bool isOn)
+        {
+            pageModel.HttpContext.Session.SetString("Unicorn", isOn ? "Yes" : "No");
             pageModel.HttpContext.Session.CommitAsync().Wait();
         }
     }
---
+++ KnotGames.ProgrammingPuzzles/Pages/OptionPage2.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KnotGames.ProgrammingPuzzles.Pages
+{
+    public class OptionPage2Model : PageModel
+    {
+        [BindProperty]
+        public bool UnicornMode { get; set; }
+
+        public void OnGet()
+        {
+            UnicornMode = this.GetUnicornMode();
+        }
+
+        public ActionResult OnPost()
+        {
+            this.SetUnicornMode(UnicornMode);
+            return RedirectToPage("StartMenu");
+        }
+    }
+}
